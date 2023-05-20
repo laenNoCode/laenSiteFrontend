dadbace54e42c8d4c8b94d89656eed03ac3606da
laennocode <youen.froger2@gmail.com>
2023-05-20 17:05:00 +0200

    update of CV in french only

--- src/components/fr/CV.vue
+++ src/components/fr/CV.vue
@@ -13,21 +13,22 @@
                 <br>
                 &gt;nationalité : Française
                 <br/>
-                &gt;âge&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; : 25 ans
+                &gt;âge&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; : 26 ans
                 
                 </td>
             </tr>
         </table>
         </span>
         
-                Ingénieur en microélectronique et developpeur.
+                Ingénieur spécialisé en Microélectronique et Informatique.
                 <br/>
                 <br/>
         <p>
             J'ai été diplomé en 2021 de l'école d'ingénieur des Mines de saint Étienne.
             <br/>
             <h1>Mes atouts</h1>
-            <p>Je sais programmer en 6 langages (Java, C/C++, C#, Python, Javascript, PHP), et je suis capable d'en utiliser plus de 5 autres. Je suis très curieux et j'apprends vite. Je suis particulièrement intéressé par les technologies les plus récentes. Je suis indépendant et capable de travailler dans une équipe. Je parle anglais courament. Je suis sensible à l'efficacité énergétique et à la cybersécurité. Je suis rigoureux. Je suis actuellement en train de me former sur le développement OS.
+            <p>
+            Je sais programmer en de nombreux langages, je suis passionné et je cherche toujours à creuser plus loin afin de comprendre le mieux possible le monde qui m'entoure, en particulier le monde de la technologique. Je suis très curieux, rigoureux et j'apprends vite, ce qui me permet d'être pluri-disciplinaire, notamment dans le domaine de l'informatique où mes compétences vont de la microélectronique et de l'électronique au développement web en passant par la robotique et la mécanique. Je suis autonome mais également capable de travailler dans une équipe. Je parle anglais couramment, ce qui m'a permis de voyager et d'échanger avec des personnes de cultures différentes. Je suis sensible à l'efficacité énergétique, à l'opensource et à la cybersécurité.
             </p>
             <br class="smallLine">
             <h1>
@@ -36,7 +37,7 @@
             <ul>
                 <li><img src="../../assets/fr.jpg" alt="drapeau de France"> 2014-2017 : Prépa PTSI-PT (Mathématique, Physique, Sciences industrielles, mécanique)</li>
                 <li><img src="../../assets/fr.jpg" alt="drapeau de France"> 2017-2020 : Mines de ST Étienne (école d'ingénieur, spécialité Microélectronique et Informatique) </li>
-                <li class="tab"><img src="../../assets/tw.jpg" alt="drapeau de Taiwan">Septembre 2019-Janvier 2020 : Échange accadémique à National Taiwanese University (Communication numérique et microélectronique avancée)</li>
+                <li class="tab"><img src="../../assets/tw.jpg" alt="drapeau de Taiwan">Septembre 2019-Janvier 2020 : Échange académique à National Taiwanese University (Communication numérique et microélectronique avancée)</li>
             </ul>
         </p>
         <p>
@@ -47,20 +48,40 @@
                 <li>2018 : Stage à Eon Reality : développement d'un framework de test automatisé pour des applications de réalité augmentée</li>
                 <li>2018-2020 : Travail à temps partiel en tant que lead developper sur TheBigQuiver, une plateforme de location de planches de surfs en ligne : développement web et management d'une équipe de 3 personnes </li>
                 <li>2021 : stage de 6 mois pour l'entreprise Fenotek : développement d'un banc d'automatisation de la configuration de leurs visiophones, scalable et correction d'erreurs. </li>
+                <li>2022-2023 : Ingénieur de Recherche à l'ENS Rennes : réalisation d'un outil en Python de répartition optimal de consommations flexibles (EMS) dans le cadre du projet ELFE (Expérimentons Localement la Flexibilité Energetique).
+                <br/>Ce logiciel a été réalisé sous la direction de Roman Le Goff Latimier et d'Hamid Ben Ahmed et en collaboration avec l'association Energie citoyenne en Pays de Vilaine (EPV) à l'initiative du projet ELFE.
+                <br/>Il s'agit de répartir la consommation des appareils de 115 citoyens volontaires ainsi que 35 bâtiments de collectivités/d'entreprises pour les mettre en adéquation avec la production d'énergie du territoire (principalement éolienne donc intermittente) et ainsi limiter les imports/exports d'énergie, donc les pertes en lignes.
+                <br/>Ce projet a été l'occasion d'apprendre à formuler un problème de gestion optimale sous contraintes d'un grand nombre d'acteurs (environ 700) pour un solveur MILP (échelle de temps 1/4 d'heure).
+                </li>
+                <li class="tab">2023 : Vacation : enseignement de l'Informatique au moyen du langage C aux premières années du cursus Mécatronique à l'ENS Rennes</li>
+            </ul>
+        </p>
+        <p>
+            <h1>
+                Projets d'étude : 
+            </h1>
+            <ul>
+                <li>2021 : Mise en place d'un composant de controle de flux d'exécution sur un FPGA simulant une architecture RISC-V dans un contexte de surface limitée et c++/VHDL/SystemVerilog</li>
+                <li>2019 : Réalisation de l'asservissement numérique d'un robot dans le cadre de la coupe de france de robotique 2019</li>
+                <li>Controle du parking d'une flotte de robots sur une carte STM32 en utilisant la communication zigbee et bluetooth</li>
             </ul>
         </p>
         <p>
             <h1>
                 Compétences connexes : 
             </h1>
-            Git, Linux, Debian, WSL, OpenGL, HTML/CSS, SQL, Trello, Vue/Angular/React, Express, microcontrôleurs, systèmes embarqués, capteurs, cybersécurité(1160 points sur root-me), concecption microélectronique, suite logicielle cadence, VHDL.
+            Environnement de développement : Git, Linux, Debian, WSL, suite logicielle cadence.
+            <br/>Langages: C/C++, Python, Javascript, Java, C# pour Unity, VHDL, assembleur, SQL, HTML/CSS, Matlab, Rust (débutant).
+            <br/>Frameworks : OpenGL, Trello, Vue/Angular/React, Express, numpy, scipy, Unity.
+            <br/>Hardware : Microcontrôleurs, systèmes embarqués, capteurs, conception microélectronique.
+            <br/>Autres : Réseau/CISCO, Cybersécurité(1160 points sur root-me), électronique, Xilinx Vivado, contrôle optimal.
         </p>
         <p>
             <h1>Langues et interêts</h1>
             Français - Anglais (courant, 910 TOEIC) - Japonais (débutant) - Chinois(débutant) - Espagnol (débutant) <br>
-            Formation musicale et percussions au conservatoire de St Malo <br>
+            Formation musicale et percussions au conservatoire de St Malo durant 9 ans<br>
             Voile, voyage à l'international, fabrication de jeux vidéos et de robots amateurs, white hat hacking.
-            Développement OS (x86/x86).
+            Développement OS (x86/x86_64).
             </p>
             
             <p class="notPrint last">
@@ -68,11 +89,13 @@
             <br>
             Vous pouvez imprimer ce CV, il ne sera pas noir
             <br>
-            <img src="../../assets/en.jpg" alt="drapeau d'Angleterre">You can also check out the english version <router-link :to="{name:'EnCV'}">here !</router-link>
         </p>
     </div>
 </template>
 <style scoped>
+li{
+    margin-top:5px
+}
 .centered{
 display: inline-block;
   margin-left: auto;
